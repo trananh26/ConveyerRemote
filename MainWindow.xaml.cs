8a5734a97a8ed562742f148bf2bc90c9b88004ba
PC <PC@DESKTOP-QODAGQI>
2023-12-10 21:19:39 +0700

    commit

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,6 +24,7 @@ using System.Data;
 using CaptureWebcam.Common;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using System.Threading;
 
 namespace CaptureWebcam
 {
@@ -37,17 +38,17 @@ namespace CaptureWebcam
             //the fps of the webcam
             int cameraFps = 30;
 
-            //init the camera
-            capture = new VideoCapture(1);
+            ////init the camera
+            //capture = new VideoCapture(1);
 
             //set the captured frame width and height (default 640x480)
-            capture.Set(CapProp.FrameWidth, 1024);
-            capture.Set(CapProp.FrameHeight, 768);
+            //capture.Set(CapProp.FrameWidth, 1024);
+            //capture.Set(CapProp.FrameHeight, 768);
 
             DispatcherTimer TimerPing = new DispatcherTimer();
             TimerPing.Interval = TimeSpan.FromSeconds(5);
             TimerPing.Tick += TimerPing_Tick;
-            TimerPing.Start();
+            //TimerPing.Start();
 
             DispatcherTimer TimerReadPLC = new DispatcherTimer();
             TimerReadPLC.Interval = TimeSpan.FromSeconds(0.1);
@@ -55,12 +56,12 @@ namespace CaptureWebcam
             TimerReadPLC.Start();
 
             //create a timer that refreshes the webcam feed
-            timer = new Timer()
+            timer = new System.Timers.Timer()
             {
                 Interval = 1000 / cameraFps,
                 Enabled = true
             };
-            timer.Elapsed += new ElapsedEventHandler(timer_Tick);
+            //timer.Elapsed += new ElapsedEventHandler(timer_Tick);
         }
 
 
@@ -68,28 +69,28 @@ namespace CaptureWebcam
         FilterInfoCollection filterInfo;
         VideoCaptureDevice captureDevice;
 
-        Timer timer;
+        System.Timers.Timer timer;
         BLDatabase oBL = new BLDatabase();
         List<clsMaterial> lstMaterial = new List<clsMaterial>();
         private ActUtlType PLC = new ActUtlType();
-        private string IP_PLC = "192.168.3.250";
+        private string IP_PLC = "192.168.1.250";
         private string _oldQRCode;
         private int psWait;
 
         private void Connect_PLC()
         {
-            try
-            {
-                PLC.ActLogicalStationNumber = 25;
-                PLC.Open();
-                PLC.SetDevice("M1", 1);
+            //try
+            //{
+            //    PLC.ActLogicalStationNumber = 25;
+            //    PLC.Open();
+            //    PLC.SetDevice("M1", 1);
 
-                MessageBox.Show("Kết nối PLC thành công", "Notification", MessageBoxButton.OK, MessageBoxImage.Information);
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("Không kết nối được với PLC. Vui lòng kiểm tra lại kết nối", "Warning", MessageBoxButton.OK, MessageBoxImage.Information);
-            }
+            //    MessageBox.Show("Kết nối PLC thành công", "Notification", MessageBoxButton.OK, MessageBoxImage.Information);
+            //}
+            //catch (Exception)
+            //{
+            //    MessageBox.Show("Không kết nối được với PLC. Vui lòng kiểm tra lại kết nối", "Warning", MessageBoxButton.OK, MessageBoxImage.Information);
+            //}
         }
         private void TimerPing_Tick(object sender, EventArgs e)
         {
@@ -140,31 +141,31 @@ namespace CaptureWebcam
         /// Có thông tin --> kệ mẹ n đẩy theo chiều cao
         private void ReadPLCParameter()
         {
-            int SS1;
-            PLC.GetDevice("M511", out SS1);
-            int SS2;
-            PLC.GetDevice("M512", out SS2);
-            int SS3;
-            PLC.GetDevice("M513", out SS3);
-            if (SS1 == 1)
-            {
-                if (txtQRCode.Text == _oldQRCode || psWait == 1)
-                {
-                    // hàng k có mã QRcode hoặc đọc lỗi
-                }
+        //    int SS1;
+        //    PLC.GetDevice("M511", out SS1);
+        //    int SS2;
+        //    PLC.GetDevice("M512", out SS2);
+        //    int SS3;
+        //    PLC.GetDevice("M513", out SS3);
+        //    if (SS1 == 1)
+        //    {
+        //        if (txtQRCode.Text == _oldQRCode || psWait == 1)
+        //        {
+        //            // hàng k có mã QRcode hoặc đọc lỗi
+        //        }
 
 
-            }
+        //    }
 
-            if (SS2 == 1)
-            {
+        //    if (SS2 == 1)
+        //    {
 
-            }
+        //    }
 
-            if (SS3 == 1)
-            {
+        //    if (SS3 == 1)
+        //    {
 
-            }
+        //    }
 
         }
 
@@ -232,7 +233,7 @@ namespace CaptureWebcam
 
                 filterInfo = new FilterInfoCollection(FilterCategory.VideoInputDevice);
 
-                captureDevice = new VideoCaptureDevice(filterInfo[1].MonikerString);
+                captureDevice = new VideoCaptureDevice(filterInfo[0].MonikerString);
                 captureDevice.NewFrame += CaptureDevice_NewFrame;
                 captureDevice.Start();
             }
@@ -246,9 +247,29 @@ namespace CaptureWebcam
         {
             this.Dispatcher.Invoke(() =>
             {
+                Bitmap img = (Bitmap)eventArgs.Frame.Clone();
+
+                this.Dispatcher.Invoke(System.Windows.Threading.DispatcherPriority.Render, (SendOrPostCallback)delegate
+                {
+                    IntPtr hBitmap = img.GetHbitmap();
+                    System.Windows.Media.Imaging.BitmapSource bitmapSource = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                        hBitmap,
+                        IntPtr.Zero,
+                        Int32Rect.Empty,
+                        System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+
+                    //DeleteObject(hBitmap);
+
+                    img.Dispose();
+                    GC.Collect();
+                    feedImage.Source = bitmapSource;
+
+                }, null);
+
 
                 string qrcode = FindQrCodeInImage((Bitmap)eventArgs.Frame.Clone());
 
+                //Image1.Source = (Bitmap)eventArgs.Frame.Clone();
                 if (!string.IsNullOrEmpty(qrcode))
                 {
                     //set the found text in the qr code in the ui
@@ -258,6 +279,7 @@ namespace CaptureWebcam
                     if (qrcode != _oldQRCode)
                     {
                         CheckMaterialInfor();
+                        _oldQRCode=qrcode;
                     }
                     //play a sound to indicate qr code found
                     var player_ok = new SoundPlayer(GetStreamFromResource("sound_ok.wav"));
@@ -288,76 +310,76 @@ namespace CaptureWebcam
             return Result;
         }
 
-        private async void timer_Tick(object sender, ElapsedEventArgs e)
-        {
-            try
-            {
-                //there is a qr code image visible
-                if (feedImage.Visibility == Visibility.Collapsed)
-                {
-                    timer.Stop();
+        //private async void timer_Tick(object sender, ElapsedEventArgs e)
+        //{
+        //    try
+        //    {
+        //        //there is a qr code image visible
+        //        if (feedImage.Visibility == Visibility.Collapsed)
+        //        {
+        //            timer.Stop();
 
-                    //the delay time you want to display the qr code in the ui for
-                    await Task.Run(() => Task.Delay(2500));
+        //            //the delay time you want to display the qr code in the ui for
+        //            await Task.Run(() => Task.Delay(2500));
 
-                    //set the image visibility
-                    this.Dispatcher.Invoke(() =>
-                    {
-                        feedImage.Visibility = Visibility.Visible;
-                        Image1.Visibility = Visibility.Collapsed;
-                    });
+        //            //set the image visibility
+        //            this.Dispatcher.Invoke(() =>
+        //            {
+        //                feedImage.Visibility = Visibility.Visible;
+        //                Image1.Visibility = Visibility.Collapsed;
+        //            });
 
-                    timer.Start();
-                }
+        //            timer.Start();
+        //        }
 
-                this.Dispatcher.Invoke(() =>
-                {
-                    var mat1 = capture.QueryFrame();
-                    var mat2 = new Mat();
+        //        this.Dispatcher.Invoke(() =>
+        //        {
+        //            var mat1 = capture.QueryFrame();
+        //            var mat2 = new Mat();
 
-                    //flip the image horizontally
-                    CvInvoke.Flip(mat1, mat2, FlipType.Horizontal);
+        //            //flip the image horizontally
+        //            CvInvoke.Flip(mat1, mat2, FlipType.Horizontal);
 
-                    //convert the mat to a bitmap
-                    var bmp = mat2.ToImage<Bgr, byte>().ToBitmap();
+        //            //convert the mat to a bitmap
+        //            var bmp = mat2.ToImage<Bgr, byte>().ToBitmap();
 
-                    //copy the bitmap to a memorystream
-                    var ms = new MemoryStream();
-                    bmp.Save(ms, ImageFormat.Bmp);
+        //            //copy the bitmap to a memorystream
+        //            var ms = new MemoryStream();
+        //            bmp.Save(ms, ImageFormat.Bmp);
 
-                    //display the image on the ui
-                    feedImage.Source = BitmapFrame.Create(ms);
+        //            //display the image on the ui
+        //            feedImage.Source = BitmapFrame.Create(ms);
 
-                    //try to find a qr code in the feed
-                    string qrcode = FindQrCodeInImage(bmp);
+        //            //try to find a qr code in the feed
+        //            string qrcode = FindQrCodeInImage(bmp);
 
-                    if (!string.IsNullOrEmpty(qrcode))
-                    {
-                        //set the found text in the qr code in the ui
-                        txtQRCode.Text = qrcode;
-                        txtCheckTime.Text = $"Last scan: {DateTime.Now.ToLongDateString()} at {DateTime.Now.ToShortTimeString()}.";
+        //            if (!string.IsNullOrEmpty(qrcode))
+        //            {
+        //                //set the found text in the qr code in the ui
+        //                txtQRCode.Text = qrcode;
+        //                txtCheckTime.Text = $"Last scan: {DateTime.Now.ToLongDateString()} at {DateTime.Now.ToShortTimeString()}.";
 
-                        if (qrcode != _oldQRCode)
-                        {
-                            CheckMaterialInfor();
-                        }
-                        //play a sound to indicate qr code found
-                        var player_ok = new SoundPlayer(GetStreamFromResource("sound_ok.wav"));
-                        player_ok.Play();
+        //                if (qrcode != _oldQRCode)
+        //                {
+        //                    CheckMaterialInfor();
+        //                }
+        //                //play a sound to indicate qr code found
+        //                var player_ok = new SoundPlayer(GetStreamFromResource("sound_ok.wav"));
+        //                player_ok.Play();
 
-                        //hide the feed image
-                        feedImage.Visibility = Visibility.Collapsed;
-                    }
+        //                //hide the feed image
+        //                feedImage.Visibility = Visibility.Collapsed;
+        //            }
 
-                });
-            }
-            catch (Exception ee)
-            {
+        //        });
+        //    }
+        //    catch (Exception ee)
+        //    {
 
-                MessageBox.Show(ee.Message);
-            }
+        //        MessageBox.Show(ee.Message);
+        //    }
 
-        }
+        //}
 
         private string FindQrCodeInImage(Bitmap bmp)
         {
